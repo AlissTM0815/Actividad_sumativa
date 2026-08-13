c09eb511078d27bf235d9b2b72078f1f5001f0eb
AlissTM0815 <26150601@aguascalientes.tecnm.mx>
2026-08-13 15:21:28 -0600

    Add files via upload

---
+++ proyectos/Actividad_Sumativa_2/area_rec.py
@@ -0,0 +1,5 @@
+#Programa para calcular el area del rectangulo
+base = float (input("Ingrese la base: "))
+altura = float (input("Ingrese la altura:"))
+area = base * altura
+print("El area del rectangulo es:", area)
