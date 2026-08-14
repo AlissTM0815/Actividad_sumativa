1bcf0be1f63dcfb23a6ca2cda4ef2de4f76260e5
AlissTM0815 <26150601@aguascalientes.tecnm.mx>
2026-08-13 18:21:54 -0600

    Add files via upload

---
+++ Actividad_Selectiva/Brindar_inf.py
@@ -0,0 +1,22 @@
+#Brindar informacion
+consulta = input("Ingrese nombre de artista, pelicula o serie: ") .lower()
+match consulta:
+    
+    case "inception":
+        info = "Pelicula de ciencia ficcion dirigida por Cristopher Nolan."
+    
+    case "beatles":
+        info = "Banda britanica de rock formada en 1960."
+
+    case "rick and morty":
+        info = "Serie animada de comedia y ciencia ficcion."
+    
+    case "stranger things":
+        info = "Serie de terror y ciencia ficcion de Netfix."
+
+    case "avengers":
+        info = "Pelicula de superheroes del MCU."
+    
+    case _:
+        info = "No se encontro informacion."
+print("Informacion", info)
